497a7713430c41fa35a3a3498558ddfb710a64d1
SergeBychkov <bychkov.sergey@gmail.com>
2024-05-27 14:32:52 +0500

    sec

---
+++ HackerRank/src/Algorithms/Strings/CommonChild.cpp
@@ -0,0 +1,66 @@
+//https://www.hackerrank.com/challenges/common-child/problem?isFullScreen=true
+
+#include <print>
+#include <vector>
+#include <algorithm>
+#include <ranges>
+#include <numeric>
+#include <chrono>
+#include "print_utils.h"
+#include "random_utils.h"
+using namespace std;
+
+namespace {
+  class Qqq {
+  public:
+    //commit
+    //commit2
+    Qqq() {}
+
+
+    int commonChild(string s1, string s2) {
+
+    }
+
+
+    int work() {
+      int result{};
+
+      return result;
+    }
+
+
+  private:
+
+  };
+
+
+
+  void check(int expected) {
+    Qqq qqq;
+    auto result = qqq.work();
+    if (result != expected) {
+      println("Error with result = {}, expected = {}", result, expected);
+      throw exception("Error!");
+    }
+  }
+}
+
+
+
+void demo_CommonChild() {
+  try {
+    println("demo_CommonChild started ...\n");
+
+    //Qqq qqq(100);
+    //auto result = qqq.work();
+    //println("result = {}", result);
+
+    check(100);
+
+    println("\nAll tests passed!");
+  } catch (exception ex) {
+    println("{}", ex.what());
+  }
+
+}
---
+++ HackerRank/src/main.cpp
@@ -0,0 +1,15 @@
+#include <print>
+using namespace std;
+
+//Algorithms/Strings/
+void demo_CommonChild();
+
+int main() {
+  try {
+    //Algorithms/Strings/
+    demo_CommonChild();
+
+  } catch (exception ex) {
+    println("{}", ex.what());
+  }
+}
